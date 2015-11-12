3c247899c99fab698a7c05a66cfad88c0e0a7061
Wilson Rondini <wilbecoding@gmail.com>
2015-11-11 18:39:07 -0700

    Fixed show/hide functionallity by creating toggle functions in controller. Slimmed things down to only one all encompassing controller

--- app.js
+++ app.js
@@ -1,27 +1,38 @@
 var app = angular.module('redditApp', []);
 
 app.controller('globalController', function($scope){
+    $scope.visible = false;
+    $scope.toggle = function(){
+        $scope.visible = (!$scope.visible)? true: false;
+    }
+
+    $scope.showCommentsVisible = false;
+    $scope.toggleVisibleComments = function(){
+        $scope.showCommentsVisible = (!$scope.showCommentsVisible)? true:false;
+    }
+
     $scope.postArray = [];
+    $scope.showComments = false;
     $scope.movePostObject = function(post){
         post.votes = Number(0);
         $scope.postArray.push(post);
         $scope.post = {};
     }
+
     $scope.upVote = function(post){
        this.post.votes++
     }
     $scope.downVote = function(post){
         this.post.votes--
     }
-});
 
-app.controller('commentController', function($scope){
     $scope.commentArray = [];
     $scope.moveCommentObject = function(comment){
         console.log('this function hits')
+        console.log(comment, 'coment in teh coment function')
         comment.amount = Number(0);
         $scope.commentArray.push(comment);
         $scope.comment = {};
-        this.comment.amount++
     }
 });
+
